bd4a1597bbea2d18f078f09b8481b445185e129a
thetalemon <sasami0314@gmail.com>
2022-12-10 23:40:30 +0900

    add twitter

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -29,6 +29,10 @@ async function render(pageContext: PageContextServer) {
         <link rel="icon" href="/mylogo.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:description" content="${desc}" />
+        <meta name="twitter:title" content="${title}" />
+        <meta name="twitter:image" content="${desc}" />
         <meta property="og:image" content="${img}" />
         <meta property="og:title" content="${title}" />
         <meta property="og:description" content="${desc}" />
