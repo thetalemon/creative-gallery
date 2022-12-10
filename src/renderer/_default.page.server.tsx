f313ed190405f4f5db106572bb3142734597c1f5
thetalemon <sasami0314@gmail.com>
2022-12-10 23:49:35 +0900

    add twitter:site

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -30,6 +30,7 @@ async function render(pageContext: PageContextServer) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@thetalemon">
         <meta name="twitter:description" content="${desc}" />
         <meta name="twitter:title" content="${title}" />
         <meta name="twitter:image" content="${desc}" />
